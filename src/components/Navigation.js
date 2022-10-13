4437e447cbe162b29b14a1276216ec43446d0f47
Ali Samir <alisamir.2020@azhar.edu.eg>
2022-10-13 09:27:27 +0200

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,78 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "./Button";
+import Logo from "./Logo";
+
+const Section = styled.section`
+  width: 100vw;
+  background-color: ${(props) => props.theme.body};
+`;
+
+const NavBar = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 85%;
+  height: ${(props) => props.theme.navHeight};
+  margin: 0 auto;
+`;
+
+const Menu = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+`;
+
+const MenuItem = styled.li`
+  margin: 0 1rem;
+  color: ${(props) => props.theme.text};
+  cursor: pointer;
+
+  &::after {
+    content: "";
+    display: block;
+    width: 0%;
+    height: 2px;
+    background: ${(props) => props.theme.text};
+    transition: width 0.3s ease;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+`;
+
+const Navigation = () => {
+  const scrollTo = (id) => {
+    let element = document.getElementById(id);
+
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
+  };
+
+  return (
+    <Section id="navigation">
+      <NavBar>
+        <Logo />
+        <Menu>
+          <MenuItem onClick={() => scrollTo("home")}>Home</MenuItem>
+          <MenuItem onClick={() => scrollTo("about")}>About</MenuItem>
+          <MenuItem onClick={() => scrollTo("roadmap")}>Roadmap</MenuItem>
+          <MenuItem onClick={() => scrollTo("showcase")}>Showcase</MenuItem>
+          <MenuItem onClick={() => scrollTo("team")}>Team</MenuItem>
+          <MenuItem onClick={() => scrollTo("faq")}>Faq</MenuItem>
+        </Menu>
+        <Button
+          text="Connect Wallet"
+          link="https://www.linkedin.com/in/ali-samir-ali/"
+        />
+      </NavBar>
+    </Section>
+  );
+};
+
+export default Navigation;
